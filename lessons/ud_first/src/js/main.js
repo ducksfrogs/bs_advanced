1a6a348736c5f5d56007e8c762f459cbf80e3e9d
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-05 04:56:52 +0900

    d'

---
+++ lessons/ud_first/src/js/main.js
@@ -0,0 +1,7 @@
+import '../scss/styles.scss'
+
+import {Tooltip, Toast, Popover} from "bootstrap";
+
+import Alert from 'bootstrap/js/dist/alert';
+// Tree-shaking Example
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
