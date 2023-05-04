f03245cb247690737075ab635359aa9a8664037e
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-04 20:54:28 +0900

    d

--- lessons/blog/src/js/main.js
+++ lessons/blog/src/js/main.js
@@ -3,11 +3,19 @@ import {Tooltip, Toast, Popover} from "bootstrap";
 import Alert from 'bootstrap/js/dist/alert';
 
 import img1 from '../img/img1_half.jpg';
-// import img2 from '../img/img2.jpg';
+import img2 from '../img/img2.jpg';
+import loopI from '../img/img4.jpg';
 
 
 const testImg = document.createElement('img');
 testImg.classList.add('card-img-top');
 testImg.src = img1;
-document.getElementById('card').appendChild(testImg);
+document.getElementsByClassName('card').appendChild(testImg);
 
+const loopImg = document.createElement('img');
+loopImg.src = loopI;
+document.getElementById('unsp').appendChild(loopImg);
+
+const test1Img = document.createElement('img');
+test1Img.src = img2;
+document.getElementById("momo").appendChild(test1Img);
